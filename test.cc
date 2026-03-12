5e20e09e7d56b66eb214d61dd31dd1c4d4ec79f0
Joel Rejive <joelrejive2@gmail.com>
2026-03-12 19:47:12 +0000

    Finished hackalong lesson

--- test.cc
+++ test.cc
@@ -1,3 +1,22 @@
 // Name: Joel Rejive
 // CWID: 883464604
 
+#include <string>
+#include <iostream>
+
+void double_it(int& x){
+    x = x * 2;
+    std::cout << "x is " << x << "\n";
+}
+
+void double_it_pointer(int* x){
+    *(x + 1) = *(x + 1) * 2;
+    std::cout << "x is " << x << "\n";
+}
+
+int main(){
+    int value [] = {5, 7, 13, 17, 19};
+    std::cout << "value is " << value[0] << "\n";
+    double_it_pointer(value);
+    std::cout << "value is " << *value << "\n";
+}
